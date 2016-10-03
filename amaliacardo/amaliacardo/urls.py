ec3a2779cfe4a92b7f8fedf6a8b68e4bdaaabe74
Antonio D'Alessio <antonio@adias.it>
2016-10-03 13:54:24 +0200

    init repository

--- amaliacardo/amaliacardo/urls.py
+++ amaliacardo/amaliacardo/urls.py
@@ -1,4 +1,4 @@
-"""sartofilia URL Configuration
+"""amaliacardo URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/1.8/topics/http/urls/
--- amaliacardo/amaliacardo/wsgi.py
+++ amaliacardo/amaliacardo/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for sartofilia project.
+WSGI config for amaliacardo project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "sartofilia.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "amaliacardo.settings")
 
 application = get_wsgi_application()
